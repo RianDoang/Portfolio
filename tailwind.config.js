1399c73abb0fa79ccc8d49987e4c025776f95a77
RianDoang <muhamadrynsyy270521@gmail.com>
2024-06-30 16:57:24 +0700

    Mengubah data diri dan mengubah animasi dibagian skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       fontFamily: {
         figtree: ["Figtree", "sans-serif"],
       },
+      animation: {
+        scaleIn: "scale .2s ease-in-out",
+      },
+      keyframes: {
+        scale: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(0.9)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
     },
   },
   plugins: ["prettier-plugin-tailwindcss"],
